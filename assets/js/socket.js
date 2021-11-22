d52711325cc927a541c7b7cb408385f46f7f7ba0
Alessanteri <alessanterio@mail.ru>
2021-11-22 16:05:27 +0300

    Fix assets

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -1,66 +1,55 @@
-// NOTE: The contents of this file will only be executed if
-// you uncomment its entry in "assets/js/app.js".
-
-// To use Phoenix channels, the first step is to import Socket,
-// and connect at the socket path in "lib/web/endpoint.ex".
-//
-// Pass the token on params as below. Or remove it
-// from the params if you are not using authentication.
+import $ from "jquery"
 import { Socket } from "phoenix"
 
-//let socket = new Socket("/socket", { params: { token: window.userToken } })
-
-// When you connect, you'll often need to authenticate the client.
-// For example, imagine you have an authentication plug, `MyAuth`,
-// which authenticates the session and assigns a `:current_user`.
-// If the current user exists you can assign the user's token in
-// the connection for use in the layout.
-//
-// In your "lib/web/router.ex":
-//
-//     pipeline :browser do
-//       ...
-//       plug MyAuth
-//       plug :put_user_token
-//     end
-//
-//     defp put_user_token(conn, _) do
-//       if current_user = conn.assigns[:current_user] do
-//         token = Phoenix.Token.sign(conn, "user socket", current_user.id)
-//         assign(conn, :user_token, token)
-//       else
-//         conn
-//       end
-//     end
-//
-// Now you need to pass this token to JavaScript. You can do so
-// inside a script tag in "lib/web/templates/layout/app.html.eex":
-//
-//     <script>window.userToken = "<%= assigns[:user_token] %>";</script>
-//
-// You will need to verify the user token in the "connect/3" function
-// in "lib/web/channels/user_socket.ex":
-//
-//     def connect(%{"token" => token}, socket, _connect_info) do
-//       # max_age: 1209600 is equivalent to two weeks in seconds
-//       case Phoenix.Token.verify(socket, "user socket", token, max_age: 1209600) do
-//         {:ok, user_id} ->
-//           {:ok, assign(socket, :user, user_id)}
-//         {:error, reason} ->
-//           :error
-//       end
-//     end
-//
-// Finally, connect to the socket:
-
-
+let socket = new Socket("/socket", { params: { token: window.userToken } })
 
 socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
-// let channel = socket.channel("topic:subtopic", {})
+let channel = socket.channel("update_data:lobby", {})
+
+
 channel.join()
-    .receive("ok", resp => { console.log("Joined successfully", resp) })
-    .receive("error", resp => { console.log("Unable to join", resp) })
+    .receive("ok", resp => {
+        console.log("Joined successfully", resp)
+
+    })
+    .receive("error", resp => {
+        console.log("Unable to join", resp)
+    })
+
+
+//let form = document.querySelector("#event-from")
+
+
+document.querySelector("#event-from").addEventListener('submit', (e) => {
+    e.preventDefault()
+    let { value: repetition } = e.target.querySelector('#repetition')
+    let { value: date_year } = e.target.querySelector('#start_date_year')
+    let { value: date_month } = e.target.querySelector('#start_date_month')
+    let { value: date_day } = e.target.querySelector('#start_date_day')
+    let { value: date_hour } = e.target.querySelector('#start_date_hour')
+    let { value: date_minute } = e.target.querySelector('#start_date_minute')
+    channel.push('rerender', { message: { repetition, date_year, date_month, date_day, date_hour, date_minute } })
+    //location.reload()
+})
+
+channel.on("update_data:lobby:new_message", (message) => {
+    console.log("message", message)
+    //var newtab = window.open('http://localhost:4000/users/2/my_schedule');
+    //location.reload();
+    //renderMessage(message)
+});
+
+
+const renderMessage = function (message) {
+    console.log("MESSAGE RENDERED", message.content.repetition)
+    location.reload()
+    let messageTemplate = `
+      <li class="list-group-item">${message.content.repetition}</li>
+    `
+    //document.querySelector("#message").innerHTML += messageTemplate
+    //document.querySelector("#ttttt").innerHTML += messageTemplate
+};
 
 export default socket
